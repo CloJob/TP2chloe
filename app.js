3c1814605495e86e29b2c394fc66e79831943160
Chloe Jobin <chloejobin.1@gmail.com>
2023-03-15 20:24:53 -0700

    tp #2 termine de base

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
+
+function randomDog(){
+    let doggieImage = document.getElementById("dogsAPI");
+    fetch ('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(json => doggieImage.src = json.message);
+}
+
+function clickBtn(){
+    let doggieBtn = document.getElementById("doggieBtn");
+    doggieBtn.addEventListener("click", randomDog);
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    randomDog();
+    clickBtn();
+})
